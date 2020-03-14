2060b7f2bbcb33b62cf232dcb130044bc6a47136
Nicholas Browdues <browdues@gmail.com>
2020-03-14 17:30:03 -0600

    Updates

---
+++ precache-manifest.c96fcfa039382c926fd53de91469a5b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050d7de565bf0906657d252531e0168b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "11839ce7e86de295ca81",
+    "url": "/monsters-rolodex/static/css/main.ddd5c9dc.chunk.css"
+  },
+  {
+    "revision": "3db869bb7de2277be351",
+    "url": "/monsters-rolodex/static/js/2.e603f8d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.e603f8d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11839ce7e86de295ca81",
+    "url": "/monsters-rolodex/static/js/main.1c20c5f6.chunk.js"
+  },
+  {
+    "revision": "c974309c68a836970a36",
+    "url": "/monsters-rolodex/static/js/runtime-main.68be9c1c.js"
+  }
+]);
